90d131b032727c52610daf3b2a9f67353376ef34
Kiran <smkiran.09@gmail.com>
2024-04-28 13:55:54 +0530

    Header Section Complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
+import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
+import NavBarItems from "../navbarItems/NavBarItems";
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src="{logo" alt="logo" />s
+          <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <NavBarItems />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up">
+            <div className="gpt3__navbar-menu_container-links">
+              <NavBarItems />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/components/navbarItems/NavBarItems.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const NavBarItems = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#gpt">What is GPT?</a>
+      </p>
+      <p>
+        <a href="#openAI">Open AI</a>
+      </p>
+      <p>
+        <a href="#caseStudies">Case Studies</a>
+      </p>
+      <p>
+        <a href="#library">Library</a>
+      </p>
+    </>
+  );
+};
+
+export default NavBarItems;
