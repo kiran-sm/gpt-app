02700e5262b9ea080d0ee2dccec08e39db3a2d20
Kiran <smkiran.09@gmail.com>
2024-04-28 14:31:23 +0530

    Brand adding completed

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,31 @@
 import React from "react";
 import "./brand.css";
+import google from "../../assets/google.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+import slack from "../../assets/slack.png";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt__brand section__padding">
+      <div>
+        <img src={google} alt="Google" />
+      </div>
+      <div>
+        <img src={slack} alt="Google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Google" />
+      </div>
+      <div>
+        <img src={shopify} alt="Google" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
